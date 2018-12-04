035a0efa3ecc1e9fa1cfb8031aff174e779dc8bb
aiime <gtfxtiro@protonmail.com>
2018-12-04 18:45:02 +0500

    Add clock

---
+++ a-ticket-from-the-bottom/Assets/ClockModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ticket.Clock
+{
+    [AddComponentMenu("Ticket/Clock/Clock Model")]
+    public class ClockModel : MonoBehaviour
+    {
+        [SerializeField] int initialHours;
+        [SerializeField] int initialMinutes;
+        [SerializeField] float minuteDuration;
+
+        public bool pause;
+
+        /// <summary>
+        /// Первый параметр - текущее количество часов, второй параметр - текущее количество минут.
+        /// </summary>
+        public Action<int, int> TimeUpdated;
+
+        int hours;
+        int minutes;
+        float currentTime;
+        Coroutine clockwork_CR;
+
+        void Awake()
+        {
+            hours = initialHours;
+            minutes = initialMinutes;
+        }
+
+        void Start()
+        {
+            if (TimeUpdated != null) TimeUpdated.Invoke(hours, minutes);
+            clockwork_CR = StartCoroutine(Clockwork());
+        }
+
+        public void StartClock()
+        {
+            if (clockwork_CR == null)
+            {
+                clockwork_CR = StartCoroutine(Clockwork());
+            }
+            else
+            {
+                pause = false;
+            }
+        }
+
+        public void PauseClock()
+        {
+            pause = true;
+        }
+
+        public void StopClock()
+        {
+            if (clockwork_CR != null)
+            {
+                StopCoroutine(clockwork_CR);
+                hours = 0;
+                minutes = 0;
+            }
+        }
+
+        IEnumerator Clockwork()
+        {
+            while (true)
+            {
+                if (!pause)
+                {
+                    currentTime += Time.deltaTime;
+
+                    if (currentTime >= minuteDuration)
+                    {
+                        minutes++;
+                        currentTime = 0;
+
+                        if (minutes == 60)
+                        {
+                            hours++;
+                            minutes = 0;
+                        }
+
+                        if (hours == 24 && minutes == 37)
+                        {
+                            hours = 0;
+                            minutes = 0;
+                        }
+
+                        if (TimeUpdated != null) TimeUpdated.Invoke(hours, minutes);
+                    }  
+                }
+
+                yield return null;
+            }
+        }
+    }
+}
+
---
+++ a-ticket-from-the-bottom/Assets/ClockView.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ticket.Clock
+{
+    [AddComponentMenu("Ticket/Clock/Clock View")]
+    [RequireComponent(typeof(ClockModel))]
+    public class ClockView : MonoBehaviour
+    {
+        [SerializeField] Text hours0;
+        [SerializeField] Text hours1;
+        [SerializeField] Text minutes0;
+        [SerializeField] Text minutes1;
+
+        ClockModel clockModel;
+
+        void Awake()
+        {
+            clockModel = GetComponent<ClockModel>();
+            clockModel.TimeUpdated += UpdateTimerDisplay;
+        }
+
+        void UpdateTimerDisplay(int hours, int minutes)
+        {
+            string hoursStr = hours.ToString().PadLeft(2, '0');
+            string minutesStr = minutes.ToString().PadLeft(2, '0');
+
+            hours0.text = hoursStr[0].ToString();
+            hours1.text = hoursStr[1].ToString();
+            minutes0.text = minutesStr[0].ToString();
+            minutes1.text = minutesStr[1].ToString();
+        }
+    }
+}
